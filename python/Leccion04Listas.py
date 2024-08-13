72b0e492f39313c16c192435f5bd9bdf8ecc0a8b
Luca Perez <lucamarper@gmail.com>
2024-08-13 10:26:26 -0300

    Comenzamos con el segundo semestre , viendo el tema de Listas en python

---
+++ python/Leccion04Listas.py
@@ -0,0 +1,10 @@
+#Lista = Ariel , Liliana, Luca,Osvaldo
+
+nombres = ['Luca' , 'Fiorella','Mariano']
+print(nombres)
+print(nombres[0]) #Mostrando Elemento que queramos
+print(nombres[1])
+print(nombres[3]) 
+print(nombres[-1]) # mostramos el ultimo numero
+print(nombres[-2]) # Mostramos el penultimo
+
