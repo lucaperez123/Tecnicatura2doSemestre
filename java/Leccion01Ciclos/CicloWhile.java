179bfd78786967cdc04066d592c289d94ed12524
Luca Perez <lucamarper@gmail.com>
2024-08-15 12:20:33 -0300

    Agregamos la carpeta de java con la leccion01 y comenzamos con el cicloWhile

---
+++ java/Leccion01Ciclos/CicloWhile.java
@@ -0,0 +1,11 @@
+public class CicloWhile{
+    public static void main(String[] args){
+        var conteo = 0; // INFERENCIA DE TIPOS
+        while(conteo<3){
+            System.out.println("conteo = " + conteo);
+            conteo++; // vamos aumentando en uno la variable
+
+
+        }
+    }
+}
