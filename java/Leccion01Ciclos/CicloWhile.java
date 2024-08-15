7362da5ebe19707a1c3852dc01f3682f56d3de2c
Luca Perez <lucamarper@gmail.com>
2024-08-15 12:32:29 -0300

    Vimos el ciclo do while su funcionamiento y aplicado a un ejercicio

--- java/Leccion01Ciclos/CicloWhile.java
+++ java/Leccion01Ciclos/CicloWhile.java
@@ -1,9 +1,17 @@
 public class CicloWhile{
     public static void main(String[] args){
+        //ciclo while
         var conteo = 0; // INFERENCIA DE TIPOS
         while(conteo<3){
             System.out.println("conteo = " + conteo);
             conteo++;
+
+        //ciclo do while
+        var contador = 0;
+        do{
+            System.out.println("contador = " + contador);
+            contador++;
+        }while(contador<7);
 
 
         }
