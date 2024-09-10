1216f82b27a7ba57da6260e8fee64249c4ff826e
Luca Perez <lucamarper@gmail.com>
2024-09-10 12:12:35 -0300

    Vimos como imprimir los objetos y agregamos metodos a objetos

--- javascript/TSLeccion04/01-04-objetos.js
+++ javascript/TSLeccion04/01-04-objetos.js
@@ -7,6 +7,16 @@ let persona = {
     apellido: 'Perez',
     email: 'juanperezgmal.com',
     edad: 28,
+    nombreCompleto: function(){ // metodo o funcion en javascript
+        return this.nombre + ' ' + this.apellido;
+    }
 }
 
 console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.nombreCompleto());
+
+
+
