221287f29e1cefb669c3ffd7e8b7e6ec0ddef3ff
Luca Perez <lucamarper@gmail.com>
2024-09-25 17:09:01 -0300

    Creacion de objetos con argumentos y creacion de mas objetos

--- python/Leccion06/Persona.py
+++ python/Leccion06/Persona.py
@@ -1,12 +1,15 @@
 class Persona #Creamos una clase
 
 
-    def__init__(self): #se lo llama metodo init dunder
-        self.nombre = "Luca"
-        self.edad = 21
-        self.apellido = "Perez"
+    def__init__(self,nombre,apellido,edad): #se lo llama metodo init dunder
+        self.nombre = nombre
+        self.edad = edad
+        self.apellido = apellido
 
-persona1 = Persona() #Constructor
+persona1 = Persona("Luca", "Perez", 21) #Constructor
 print(persona1.nombre)) #<class 'type'>
 print(persona1.apellido) #<class 'type'>
 print(persona1.edad) #<class 'type'>
+
+persona2 = Persona("Fiorella", "Gonzalez", 22)
+print(f"el objeto2 de la clase persona: {persona2.nombre} {persona2.apellido} {persona2.edad}")
