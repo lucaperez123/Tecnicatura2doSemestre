94de92774e7770bd7411907ed1344e29e827213d
Luca Perez <lucamarper@gmail.com>
2024-09-25 17:05:05 -0300

    Creacion de un metodo y objeto en python

---
+++ python/Leccion06/Persona.py
@@ -0,0 +1,12 @@
+class Persona #Creamos una clase
+
+
+    def__init__(self): #se lo llama metodo init dunder
+        self.nombre = "Luca"
+        self.edad = 21
+        self.apellido = "Perez"
+
+persona1 = Persona() #Constructor
+print(persona1.nombre)) #<class 'type'>
+print(persona1.apellido) #<class 'type'>
+print(persona1.edad) #<class 'type'>
