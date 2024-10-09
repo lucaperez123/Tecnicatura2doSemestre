e4bbb45090359176e6396189dc9c9082c16b8817
Luca Perez <lucamarper@gmail.com>
2024-10-09 20:34:58 -0300

    Agregamos componente proyectos

--- Portafolio/src/App.vue
+++ Portafolio/src/App.vue
@@ -4,7 +4,7 @@ import navBar from './components/icons/navBar.vue'
 import DatosPersonales from './components/icons/DatosPersonales.vue';
 import Educacion from './components/icons/Educacion.vue';
 import Experiencia from './components/icons/Experiencia.vue';
-
+import ProyectosComponente from './components/icons/ProyectosComponente.vue';
 </script>
 
 <template>
@@ -21,8 +21,9 @@ import Experiencia from './components/icons/Experiencia.vue';
     <section id="Educacion"><h2>Educacion-cursos</h2></section>
     <Educacion/>
     <section id="Experiencia"><h2>Experiencia</h2></section>
-     <Experiencia/>
+      <Experiencia/>
     <section id="Proyectos"><h2>Proyectos</h2></section>
+      <ProyectosComponente/>
     <section id="Habilidades"><h2>Habilidades</h2></section>
     <section id="Intereses"><h2>Intereses</h2></section>
   </main>
